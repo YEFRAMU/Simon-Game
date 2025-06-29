6d9c19ecd6570c9b81bbb78e15e0617f5302e699
YEFRAMU <99255803+YEFRAMU@users.noreply.github.com>
2025-06-29 21:39:32 +0800

    Update game.js
    added check answer for last length
    but needs debugging about checking userclickedPattern and gamePattern

--- game.js
+++ game.js
@@ -1,4 +1,7 @@
 let gamePattern = [];
+let userclickedPattern = [];
+let level = 1;
+let started = false;
 
 buttonColours = ["red", "blue", "green", "yellow"];
 
@@ -8,11 +11,9 @@ function nextSequence(){
     flashButton(chosenColour);
     playSound(chosenColour);
     console.log("Game pattern:", gamePattern);
-    console.log("Next color:", chosenColour);
 };
 
 function flashButton(chosenColour) {
-    console.log("Flashing button:", chosenColour);
     $("#" + chosenColour).fadeOut(100).fadeIn(100);
 }
 
@@ -21,4 +22,44 @@ function playSound(chosenColour) {
     sound.play();
 }
 
-nextSequence();
+function animatePress(userChosenColour){
+    $("#" + userChosenColour).addClass("pressed");
+    setTimeout(function() {
+        $("#" + userChosenColour).removeClass("pressed");
+    }, 100);
+}
+
+$(".btn").click(function() {
+    let userChosenColour = $(this).attr("id");
+    userclickedPattern.push(userChosenColour);
+    console.log(userclickedPattern);
+    playSound(userChosenColour);
+    animatePress(userChosenColour)
+    checkAnswer();
+});
+
+function checkAnswer() {
+    let currentIndex = userclickedPattern.length -1;
+
+    if (userclickedPattern[currentIndex] === gamePattern[currentIndex]){
+        if(userclickedPattern.at(-1) === gamePattern.at(-1)){
+        console.log("Correct!");
+        level++
+        $("#level-title").text("level " + level)
+        setTimeout(()=> nextSequence(), 1000);
+    }
+    }else{
+        console.log("Wrong!");
+    }
+}
+
+$(document).keypress(function() { 
+    if(!started) {
+        $("#level-title").text("level " + level)
+        console.log("Key pressed, starting game");
+        nextSequence();
+        
+        started = true;
+    }
+});
+
